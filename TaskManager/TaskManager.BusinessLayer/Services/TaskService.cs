c7789ab0be8b2f297d615670beef8a4b94ea99d3
varsha_kone <kone.varsha@gmail.com>
2020-07-29 20:44:57 +0530

    Revised Task Manager code solution

---
+++ TaskManager/TaskManager.BusinessLayer/Services/TaskService.cs
@@ -0,0 +1,139 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.BusinessLayer.Interface;
+using TaskManager.DataLayer;
+using TaskManager.Entities;
+using TaskStatus = TaskManager.Entities.TaskStatus;
+
+namespace TaskManager.BusinessLayer.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly IMongoDBContext _mongoDBContext;
+        private readonly IMongoCollection<TaskItem> _mongoCollection;
+
+        private readonly IMongoCollection<TaskGroup> _mongoCollectionGroup;
+        public TaskService(IMongoDBContext mongoDBContext)
+        {
+            _mongoDBContext = mongoDBContext;
+            _mongoCollection = _mongoDBContext.GetCollection<TaskItem>(typeof(TaskItem).Name);
+            _mongoCollectionGroup = _mongoDBContext.GetCollection<TaskGroup>(typeof(TaskGroup).Name);
+
+        }
+        public long EditTask(TaskItem task)
+        {
+            try
+            {
+                long result = 0;
+                var filterCriteria = Builders<TaskItem>.Filter.Eq("Name", task.Name);
+
+                var updateElements = Builders<TaskItem>.Update.Set("Priority",task.Priority).Set("TaskStatus", task.TaskStatus).Set("TaskStartDate", task.TaskStartDate).Set("TaskEndDate", task.TaskEndDate.AddDays(5)).Set("TaskColorCode", task.TaskColorCode);
+            
+                
+                var updateResult =_mongoCollection.UpdateOne(filterCriteria, updateElements,null);
+                if(updateResult.IsAcknowledged)
+                {
+                    result = updateResult.ModifiedCount;
+                }
+                
+                return result;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public List<TaskItem> GetAllTask()
+        {
+            try
+            {
+                var LstTask = _mongoCollection.Find(FilterDefinition<TaskItem>.Empty).ToList();
+                return LstTask;
+            }
+            catch(Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public List<TaskGroup> GetAllTaskGroup()
+        {
+            try
+            {
+                var LstGroups = _mongoCollectionGroup.Find(FilterDefinition<TaskGroup>.Empty).ToList();
+                return LstGroups;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public TaskDashboard GetDashboard()
+        {
+            try
+            {
+                int completedTask = 0;
+                int pendingTask = 0;
+                var LstGroups = _mongoCollectionGroup.Find(FilterDefinition<TaskGroup>.Empty).ToList();
+                var LstTask = _mongoCollection.Find(FilterDefinition<TaskItem>.Empty).ToList();
+                TaskDashboard dashboard = new TaskDashboard();
+                dashboard.TotalGroups = LstGroups.Count;
+                dashboard.TotalTask = LstTask.Count;
+                LstTask.ForEach(item =>
+                {
+                    if (item.TaskStatus == TaskStatus.Finished)
+                    {
+                        completedTask++;
+                    }
+                    else if (item.TaskStatus == TaskStatus.On_Hold || item.TaskStatus == TaskStatus.Progress || item.TaskStatus == TaskStatus.Yet_To_Start)
+                    {
+                        pendingTask++;
+                    }
+                });
+                dashboard.CompletedTask = completedTask;
+                dashboard.PendingTask = pendingTask;
+
+                return dashboard;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public string NewTask(TaskItem newtask)
+        {
+            try
+            {
+                
+                newtask.TaskEndDate = newtask.TaskStartDate.AddDays(5);
+                _mongoCollection.InsertOne(newtask);
+                return "New Task Added";
+            }
+
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public string NewTaskGroup(TaskGroup taskGroup)
+        {
+            try
+            {
+                _mongoCollectionGroup.InsertOne(taskGroup);
+                return "New Group Added";
+
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.BusinessLayer.Interface;
+using TaskManager.Entities;
+
+namespace TaskManager.Service.Controllers
+{
+    
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+        [Route("api/Task/test")]
+        [HttpGet]
+        public ActionResult<String> testlink()
+        {
+            try
+            {
+             
+                return "Hi";
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+        [Route("api/Task/newtask")]
+        [HttpPost]
+        public async Task<ActionResult<String>> NewTask(TaskItem newtask)
+        {
+            try
+            {
+                var result =_taskService.NewTask(newtask);
+                return result;
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+
+        [Route("api/Task/newgroup")]
+        [HttpPost]
+        public ActionResult<String> NewTaskGroup(TaskGroup newgroup)
+        {
+            try
+            {
+                var resultGroup = _taskService.NewTaskGroup(newgroup);
+                return resultGroup;
+              
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+
+        [Route("api/Task/edittask")]
+        [HttpPost]
+        public async Task<ActionResult<long>> EditTask(TaskItem task)
+        {
+            try
+            {
+                var resultEdit = _taskService.EditTask(task);
+                return resultEdit;
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+
+        [Route("api/Task/alltask")]
+        [HttpPost]
+        public  ActionResult<List<TaskItem>> GetAllTask()
+        {
+            try
+            {
+                var resultLstTask = _taskService.GetAllTask();
+                return resultLstTask ;
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+
+        [Route("api/Task/dashboard")]
+        [HttpPost]
+        public async Task<ActionResult<TaskDashboard>> GetTaskDashboard()
+        {
+            try
+            {
+
+                var resultDashboard = _taskService.GetDashboard();
+                return resultDashboard;
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+        [Route("api/Task/allgroups")]
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<TaskGroup>>> GetAllTaskGroups()
+        {
+            try
+            {
+                var resultGroups = _taskService.GetAllTaskGroup();
+                return resultGroups;
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.ToString());
+            }
+
+        }
+    }
+}
